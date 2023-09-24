505212ef3ec1a5bb9f893ead3f06ca80d4ad00ea
Miguel Flores <miguelfloreoficial@gmail.com>
2023-09-24 19:16:38 -0300

    fit: agregue un controller

--- Documents/NetBeansProjects/jpaprueba/src/main/java/com/mycompany/jpaprueba/Jpaprueba.java
+++ Documents/NetBeansProjects/jpaprueba/src/main/java/com/mycompany/jpaprueba/Jpaprueba.java
@@ -1,8 +1,12 @@
 package com.mycompany.jpaprueba;
 
+import com.mycompany.jpaprueba.persistencia.ControladoraPersistecia;
+
 public class Jpaprueba {
 
     public static void main(String[] args) {
+        
+        ControladoraPersistecia controlPersis = new ControladoraPersistecia();
         
     }
 }
---
+++ Documents/NetBeansProjects/jpaprueba/src/main/java/com/mycompany/jpaprueba/persistencia/ControladoraPersistecia.java
@@ -0,0 +1,7 @@
+package com.mycompany.jpaprueba.persistencia;
+
+public class ControladoraPersistecia {
+    //hacemos nuestra persistencia
+    AlumnoJpaController aluJpa = new AlumnoJpaController();
+    
+}
