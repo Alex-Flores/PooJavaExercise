a0040939755e5669c06cf29c246b6b34ce9d4cf5
Miguel Flores <miguelfloreoficial@gmail.com>
2023-10-10 11:55:43 -0300

    fix: ejercicio integrador

---
+++ Documents/NetBeansProjects/PeluqueriaCanina/src/main/java/com/mycompany/peluqueriacanina/PeluqueriaCanina.java
@@ -0,0 +1,13 @@
+package com.mycompany.peluqueriacanina;
+
+public class PeluqueriaCanina {
+
+    public static void main(String[] args) {
+        /*
+            MODELO DE CAPAS
+                LA CAPA DE LA PERSISTENCIA - BD
+                LA CAPA DE INTERFAZ GTRAFICA DE USUARIO - IGU
+                LA CAPA LOGICA, LAS CLASES METODOS NECESARIOS
+        */
+    }
+}
